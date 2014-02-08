07854266c3b44b49f120649e3452c0696e6134a1
8pockets <e.pockets@hotmail.co.jp>
2014-02-09 01:06:24 +0900

    redesign

---
+++ js/info.js
@@ -0,0 +1,85 @@
+function overview(id){
+	console.log('Running...');
+	var foodtype = id;
+	var venues_id =[];
+	$(".ui-block-a > a").addClass('ui-btn-active');
+	navigator.geolocation.getCurrentPosition(
+		function getvenue(position){
+			$("#foursquare").text("loading...");
+			var search = 'https://api.foursquare.com/v2/venues/search?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201&ll='+position.coords.latitude+','+position.coords.longitude;
+			//var search = 'https://api.foursquare.com/v2/venues/search?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201&ll='+35.66620055003081+','+139.7312879562378;
+			
+			$.ajax({
+				type: 'GET',
+				async: true,
+				url: search,
+				dataType: 'json',
+				data:{
+					limit:30,
+					categoryId:foodtype,
+					radius:500
+				},
+				success: function(data, textStatus, jqXHR){
+				$("#foursquare").text("");
+				console.log('Get Geolocation');
+				$.each(data.response.venues,function(i){
+
+					venues_id.push(data.response.venues[i].id);
+					var venues_name = data.response.venues[i].name;
+					var venues_distance = data.response.venues[i].location.distance;
+					var venues_count = data.response.venues[i].stats.usersCount;
+					
+					var venues_lat = data.response.venues[i].location.lat;
+					var venues_lng = data.response.venues[i].location.lng;
+					var venues_address = data.response.venues[i].location.address;
+					var venues_crossStreet = data.response.venues[i].location.crossStreet;
+
+					$("#foursquare").append(
+						$("<div>").addClass(venues_id[i]).append(
+							$("<div>").addClass("place-name").append(venues_name)).append(
+							$("<div>").addClass("place-distance").append(venues_distance).append("m")).append(
+							$("<div>").addClass("place-count").append(venues_count)));
+					getphoto(i,venues_id);
+				})//end of each
+			},//end of success
+			error: function(data, textStatus, jqXHR) {
+				$("#foursquare").text("位置情報取得でエラーが発生しました。再度ブラウザを読み込んでください。");
+			}
+		});//end of ajax			
+		}//end of function getvenue
+		
+	);
+}
+function getphoto(i,venues_id){
+		$.ajax({
+			type: 'GET',
+			async: true,
+			url:'https://api.foursquare.com/v2/venues/'+venues_id[i]+'/photos?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201',
+			dataType:'json',
+			success: function(data, textStatus, jqXHR){
+				if (data.response.photos.count != false){
+					var photo = data.response.photos.items[0].suffix;
+					var url = 'https://irs0.4sqi.net/img/general/148x148'+photo;	
+					$('#foursquare .'+venues_id[i]).prepend(
+					$("<div>").addClass("image").append(
+					$("<a>").attr("href", 'info.html').attr("target", '_blank').append(
+					$("<img>").attr("src", url).attr("onclick",'info('+venues_id[i]+')'))));
+				}else{
+					var undefied_photo = 'img/undefiened.png';	
+					$('#foursquare .'+venues_id[i]).prepend(
+					$("<div>").addClass("image").append(
+					$("<img>").attr("src", undefied_photo)));
+				}//end of if
+			},//end of success
+			error: function(data, textStatus, jqXHR) {
+				$("#foursquare").text("写真取得でエラーが発生しました。再度ブラウザを読み込んでください。");
+			}
+	});//end of ajax
+}
+function btn_active(id){
+	if(id = 1){$(".ui-block-a > a").addClass('ui-btn-active');}
+	else if(id = 2){$(".ui-block-b > a").addClass('ui-btn-active');}
+	else if(id = 3){$(".ui-block-c > a").addClass('ui-btn-active');}
+	else if(id = 4){$(".ui-block-d > a").addClass('ui-btn-active');}
+	else {$(".ui-block-e > a").addClass('ui-btn-active');}
+}
--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function overview(id){
 		function getvenue(position){
 			$("#foursquare").text("loading...");
 			var search = 'https://api.foursquare.com/v2/venues/search?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201&ll='+position.coords.latitude+','+position.coords.longitude;
-			//var search = 'https://api.foursquare.com/v2/venues/search?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201&ll='+35.66620055003081+','+139.7312879562378;
+			//var search = 'https://api.foursquare.com/v2/venues/search?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201&ll=35.66596084859448,139.7313094139099';
 			
 			$.ajax({
 				type: 'GET',
@@ -19,58 +19,103 @@ function overview(id){
 					radius:500
 				},
 				success: function(data, textStatus, jqXHR){
-				$("#foursquare").text("");
-				$.each(data.response.venues,function(i){
+					$("#foursquare").text("");
+					console.log('Get Geolocation');
+					$.each(data.response.venues,function(i){
+	
+						venues_id.push(data.response.venues[i].id);
+						var venues_name = data.response.venues[i].name,
+							venues_distance = data.response.venues[i].location.distance,
+							venues_count = data.response.venues[i].stats.usersCount;
 
-					venues_id.push(data.response.venues[i].id);
-					var venues_name = data.response.venues[i].name;
-					var venues_distance = data.response.venues[i].location.distance;
-					var venues_count = data.response.venues[i].stats.usersCount;
-					
-					var venues_lat = data.response.venues[i].location.lat;
-					var venues_lng = data.response.venues[i].location.lng;
-					var venues_address = data.response.venues[i].location.address;
-					var venues_crossStreet = data.response.venues[i].location.crossStreet;
-
-					$("#foursquare").append(
+						var venues_lat = data.response.venues[i].location.lat,
+							venues_lng = data.response.venues[i].location.lng,
+							venues_address = data.response.venues[i].location.address,
+							venues_crossStreet = data.response.venues[i].location.crossStreet;
+	
+						$("#foursquare").append(
 						$("<div>").addClass(venues_id[i]).append(
-							$("<div>").addClass("place-name").append(venues_name)).append(
-							$("<div>").addClass("place-distance").append(venues_distance).append("m")).append(
-							$("<div>").addClass("place-count").append(venues_count)));
-					getphoto(i,venues_id);
-				})//end of each
-			},//end of success
-			error: function(data, textStatus, jqXHR) {
-				$("#foursquare").text("位置情報取得でエラーが発生しました。再度ブラウザを読み込んでください。");
-			}
-		});//end of ajax			
-		}//end of function getvenue
-		
-	);
-}
-	function getphoto(i,venues_id){
-			$.ajax({
-				type: 'GET',
-				async: true,
-				url:'https://api.foursquare.com/v2/venues/'+venues_id[i]+'/photos?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201',
-				dataType:'json',
-				success: function(data, textStatus, jqXHR){
-					if (data.response.photos.count != false){
-						var photo = data.response.photos.items[0].suffix;
-						var url = 'https://irs0.4sqi.net/img/general/148x148'+photo;	
-						$('#foursquare .'+venues_id[i]).prepend(
-						$("<div>").addClass("image").append(
-						$("<a>").attr("href", '').append(
-						$("<img>").attr("src", url).attr("onclick",'info('+venues_id[i]+')'))));
-					}else{
-						var undefied_photo = 'img/undefiened.png';	
-						$('#foursquare .'+venues_id[i]).prepend(
-						$("<div>").addClass("image").append(
-						$("<img>").attr("src", undefied_photo)));
-					}//end of if
+						$("<div>").addClass("place-name").append(venues_name)).append(
+						$("<div>").addClass("place-distance").append(venues_distance).append("m")).append(
+						$("<div>").addClass("place-count").append(venues_count)));
+	
+						getphoto(i,venues_id);
+					})//end of each
 				},//end of success
 				error: function(data, textStatus, jqXHR) {
-					$("#foursquare").text("写真取得でエラーが発生しました。再度ブラウザを読み込んでください。");
+					$("#foursquare").text("位置情報取得でエラーが発生しました。再度ブラウザを読み込んでください。");
 				}
-		});//end of ajax
-	}
+			});//end of ajax
+		}//end of function getvenue
+	);
+}
+
+function getphoto(i,venues_id){
+	$.ajax({
+		type: 'GET',
+		async: true,
+		url:'https://api.foursquare.com/v2/venues/'+venues_id[i]+'/photos?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201',
+		dataType:'json',
+		success: function(data, textStatus, jqXHR){
+			if (data.response.photos.count != false){
+				var prefix = data.response.photos.items[0].prefix;
+				var suffix = data.response.photos.items[0].suffix;
+				var url = prefix+'148x148'+suffix;
+
+				$('#foursquare .'+venues_id[i]).prepend(
+				$("<div>").addClass("image").append(
+				$("<a>").attr("href", 'info.html').append(
+				//$("<a>").attr("href", 'javascript:void(0)').attr("onclick", "info('"+venues_id[i]+"')").append(
+				$("<img>").attr("src", url))));
+			}else{
+				var undefied_photo = 'img/undefiened.png';
+				$('#foursquare .'+venues_id[i]).prepend(
+				$("<div>").addClass("image").append(
+				$("<img>").attr("src", undefied_photo)));
+			}//end of if
+		},//end of success
+		error: function(data, textStatus, jqXHR) {
+			$("#foursquare").text("写真取得でエラーが発生しました。再度ブラウザを読み込んでください。");
+		}
+	});//end of ajax
+}
+
+function info(venue_id){
+	console.log(venue_id);
+	$.ajax({
+		type: 'GET',
+		async: true,
+		url:'https://api.foursquare.com/v2/venues/'+venue_id+'/photos?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201',
+		dataType:'json',
+		success: function(data, textStatus, jqXHR){
+			if (data.response.photos.count != false){
+				for(var c=0;c<=30;c++){
+					var prefix = data.response.photos.items[c].prefix;
+					var suffix = data.response.photos.items[c].suffix;
+					var info_url = prefix+'148x148'+suffix;
+					$('#info_photo').append(
+					$("<div>").addClass("image").append(
+					$("<img>").attr("src", info_url)));
+				}
+			}else{
+				var undefied_photo = 'img/undefiened.png';
+				$('#info_photo').append(
+				$("<div>").addClass("image").append(
+				$("<img>").attr("src", undefied_photo)));
+			}//end of if
+		},//end of success
+		error: function(data, textStatus, jqXHR) {
+			$("#foursquare").text("写真取得でエラーが発生しました。再度ブラウザを読み込んでください。");
+		}
+	});//end of ajax
+}
+
+/*
+function btn_active(id){
+	if(id = 1){$(".ui-block-a > a").addClass('ui-btn-active');}
+	else if(id = 2){$(".ui-block-b > a").addClass('ui-btn-active');}
+	else if(id = 3){$(".ui-block-c > a").addClass('ui-btn-active');}
+	else if(id = 4){$(".ui-block-d > a").addClass('ui-btn-active');}
+	else {$(".ui-block-e > a").addClass('ui-btn-active');}
+}
+*/
