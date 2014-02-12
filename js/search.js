c4c346534006363cd5686f3ff9384d983846f031
8pockets <e.pockets@hotmail.co.jp>
2014-02-12 19:08:42 +0900

    geolocation-useful

--- js/search.js
+++ js/search.js
@@ -1,8 +1,8 @@
 function form(id){
 	var text = location.search.substring(1).split('=');
-		venue_name = decodeURI(text[1]);
-		console.log(venue_name);
+	var venue_name = decodeURI(text[1]);
 	var foodtype = id;
+
 	$.ajax({
 		type: 'GET',
 		async: true,
@@ -10,10 +10,22 @@ function form(id){
 		url:'http://maps.googleapis.com/maps/api/geocode/json?address='+venue_name+'&sensor=false',
 		dataType:'json',
 		success: function(data, textStatus, jqXHR){
-		console.log(data.results[0].geometry.location.lat);
-		var lat = data.results[0].geometry.location.lat;
-		var lng = data.results[0].geometry.location.lng;
-		getvenue(foodtype, lat, lng);
+		console.log(data.results.length);
+		if (data.results.length == 1){
+			var lat = data.results[0].geometry.location.lat;
+			var lng = data.results[0].geometry.location.lng;
+			getvenue(foodtype, lat, lng);
+		}else{
+			console.log('ok');
+			for(var k=0;k<data.results.length;k++){
+				var lat = data.results[k].geometry.location.lat;
+				var lng = data.results[k].geometry.location.lng;
+				var address = data.results[k].formatted_address;
+				$('#anywhere').append(
+				$("<div>").addClass("selectplace").append(
+				$("<a>").attr("href", 'javascript:void(0)').attr("onclick", "getvenue('4d4b7105d754a06374d81259',"+lat+","+lng+")").attr("data-ajax", 'false').append(address)));
+			}
+		}
 		},//end of success
 		error: function(data, textStatus, jqXHR) {
 			console.log('取得に失敗');
@@ -24,6 +36,7 @@ function form(id){
 function getvenue(foodtype, lat, lng){
 	var search = 'https://api.foursquare.com/v2/venues/search?client_id=YFVDPCILKUGXBIPYONW1YQCNF1GOXTUUD3QR2MEUZITSDO50&client_secret=UTCBXKMCHNO2VHY5AABMD4TZ53EBB5GG0H1BLDXENO1GERM2&v=20140201&ll='+lat+','+lng;
 	var venues_id =[];
+	
 
 	$.ajax({
 		type: 'GET',
@@ -57,6 +70,7 @@ function getvenue(foodtype, lat, lng){
 				$("<div>").addClass("place-count").append(venues_count)));
 
 				getphoto(i,venues_id,venues_name,venues_lat,venues_lng);
+				$("#anywhere").text("");
 			})//end of each
 		},//end of success
 		error: function(data, textStatus, jqXHR) {
